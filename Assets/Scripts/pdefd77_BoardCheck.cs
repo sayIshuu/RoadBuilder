f9064055209b4c9bf5a90a9bf07281d27183a17f
sayishuu <smile52673@gmail.com>
2025-03-04 21:33:17 +0900

    dfs 추적 리스트 path 추가.

--- Assets/Scripts/pdefd77_BoardCheck.cs
+++ Assets/Scripts/pdefd77_BoardCheck.cs
@@ -1,5 +1,7 @@
 using UnityEngine;
 using TMPro;
+using NUnit.Framework;
+using System.Collections.Generic;
 
 public class pdefd77_BoardCheck : MonoBehaviour
 {
@@ -8,8 +10,19 @@ public class pdefd77_BoardCheck : MonoBehaviour
     [SerializeField]
     private TextMeshProUGUI gameOverTxt;
     private int score = 0;
+    //dfs추적을 위한 list. 각 int값으로 arr의 인덱스값이 들어갑니다.
+    private List<(int, int)> path = new List<(int, int)>();
+    private bool[,] visited = new bool[7, 7];
 
-    public static int[,] arr = new int[7, 7] { { 0, 4, 4, 4, 4, 4, 0 }, { 2, 0, 0, 0, 0, 0, 8 }, { 2, 0, 0, 0, 0, 0, 8 }, { 2, 0, 0, 0, 0, 0, 8 }, { 2, 0, 0, 0, 0, 0, 8 }, { 2, 0, 0, 0, 0, 0, 8 }, { 0, 1, 1, 1, 1, 1, 0 } };
+    public static int[,] arr = new int[7, 7] { 
+                                    { 0, 4, 4, 4, 4, 4, 0 }, 
+                                    { 2, 0, 0, 0, 0, 0, 8 }, 
+                                    { 2, 0, 0, 0, 0, 0, 8 }, 
+                                    { 2, 0, 0, 0, 0, 0, 8 }, 
+                                    { 2, 0, 0, 0, 0, 0, 8 }, 
+                                    { 2, 0, 0, 0, 0, 0, 8 }, 
+                                    { 0, 1, 1, 1, 1, 1, 0 } 
+    };
 
     public void check()
     {
@@ -19,7 +32,7 @@ public class pdefd77_BoardCheck : MonoBehaviour
             {
                 if (i != 0 && i != 6 && j != 0 && j != 6) continue;
 
-                int val = dfs(i, j, 0, 0);
+                int val = dfs(i, j, 0);
 
                 if (val > 0)
                 {
@@ -29,6 +42,7 @@ public class pdefd77_BoardCheck : MonoBehaviour
         }
     }
 
+    /*
     private int dfs(int y, int x, int prev, int len)
     {
         if (prev != 1 && y < 6 && (arr[y, x] & 4) > 0 && (arr[y + 1, x] & 1) > 0)
@@ -81,6 +95,72 @@ public class pdefd77_BoardCheck : MonoBehaviour
 
         return 0;
     }
+    */
+
+    private int dfs(int y, int x, int prev)
+    {
+        // 이미 방문한 경우 탐색 종료
+        if (visited[y, x]) return 0;
+
+        // 현재 위치 방문 표시 및 경로 저장
+        visited[y, x] = true;
+        path.Add((y, x));
+
+        if (prev != 1 && y < 6 && (arr[y, x] & 4) > 0 && (arr[y + 1, x] & 1) > 0)
+        {
+            if (y + 1 == 6)
+            {
+                return path.Count;
+            }
+            else
+            {
+                return dfs(y + 1, x, 4);
+            }
+        }
+
+        if (prev != 2 && x < 6 && (arr[y, x] & 8) > 0 && (arr[y, x - 1] & 2) > 0)
+        {
+            if (x - 1 == 0)
+            {
+                return path.Count;
+            }
+            else
+            {
+                return dfs(y, x - 1, 8);
+            }
+        }
+
+        if (prev != 4 && y > 0 && (arr[y, x] & 1) > 0 && (arr[y - 1, x] & 4) > 0)
+        {
+            if (y - 1 == 0)
+            {
+                return path.Count;
+            }
+            else
+            {
+                return dfs(y - 1, x, 1);
+            }
+        }
+
+        if (prev != 8 && x > 0 && (arr[y, x] & 2) > 0 && (arr[y, x + 1] & 8) > 0)
+        {
+            if (x + 1 == 6)
+            {
+                return path.Count;
+            }
+            else
+            {
+                return dfs(y, x + 1, 2);
+            }
+        }
+
+        // 탐색 종료 후 방문한 경로 초기화
+        visited[y, x] = false;
+        path.RemoveAt(path.Count - 1);
+
+        return 0;
+    }
+
 
     private void destroyTile(int y, int x)
     {

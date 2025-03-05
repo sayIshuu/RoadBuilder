ac3caf8e293d9a63af2b5fb69f0d63936799f3a0
sayishuu <smile52673@gmail.com>
2025-03-05 11:01:51 +0900

    선 완성 시 파괴되는 로직 추가

--- Assets/Scripts/pdefd77_BoardCheck.cs
+++ Assets/Scripts/pdefd77_BoardCheck.cs
@@ -15,7 +15,8 @@ public class pdefd77_BoardCheck : MonoBehaviour
     public int displayedTileCount = 0;
     //dfs추적을 위한 list. 각 int값으로 arr의 인덱스값이 들어갑니다.
     private List<(int, int)> path = new List<(int, int)>();
-    private bool[,] visited = new bool[7, 7];
+    //어떤 아이템을 그 칸에 들어있는지 나타내는 배열. 0 : 빈칸, 1 : 리롤+1, 2 : 타일제거
+    private int[,] item = new int[7, 7];
     //배율 변수. 길이에 따라 얼마나 점수 증폭될 지
     [SerializeField]
     private int scoreMultiplier = 1;
@@ -31,13 +32,6 @@ public class pdefd77_BoardCheck : MonoBehaviour
         {
             boardSlot[i] = boardInventory.transform.GetChild(i).gameObject;
         }
-        for (int i = 0; i < 7; i++)
-        {
-            for (int j = 0; j < 7; j++)
-            {
-                visited[i, j] = false;
-            }
-        }
     }
 
     public void check()
@@ -48,7 +42,8 @@ public class pdefd77_BoardCheck : MonoBehaviour
             {
                 if (i != 0 && i != 6 && j != 0 && j != 6) continue;
 
-                int val = dfs(i, j, 0,0);
+                path.Clear();
+                int val = dfs(i, j, 0);
 
                 if (val > 0)
                 {
@@ -56,21 +51,11 @@ public class pdefd77_BoardCheck : MonoBehaviour
                     {
                         gameOverTxt.text = "Your Score is " + score;
                     }
-                    
+
                     //gameOverTxt.gameObject.SetActive(true);
                     //gameOverTxt.text = "Your length is " + val;
-                    score += val;
-                    for (int y = 1; y < 7; y++)
-                    {
-                        for (int x = 1; x < 7; x++)
-                        {
-                            if (visited[y, x])
-                            {
-                                destroyTile(y, x);
-                                visited[y, x] = false;
-                            }
-                        }
-                    }
+
+                    getScore(val);
                 }
             }
         }
@@ -78,81 +63,20 @@ public class pdefd77_BoardCheck : MonoBehaviour
         scoreTxt.text = "Score : " + score;
     }
 
-    private int dfs(int y, int x, int prev, int len)
-    {
-        if (prev != 1 && y < 6 && (arr[y, x] & 4) > 0 && (arr[y + 1, x] & 1) > 0)
-        {
-            if (y + 1 == 6)
-            {
-                return len;
-            }
-            else
-            {
-                return dfs(y + 1, x, 4, len + 1);
-            }
-        }
 
-        if (prev != 2 && x > 0 && (arr[y, x] & 8) > 0 && (arr[y, x - 1] & 2) > 0)
-        {
-            if (x - 1 == 0)
-            {
-                return len;
-            }
-            else
-            {
-                return dfs(y, x - 1, 8, len + 1);
-            }
-        }
-
-        if (prev != 4 && y > 0 && (arr[y, x] & 1) > 0 && (arr[y - 1, x] & 4) > 0)
-        {
-            if (y - 1 == 0)
-            {
-                return len;
-            }
-            else
-            {
-                return dfs(y - 1, x, 1, len + 1);
-            }
-        }
-
-        if (prev != 8 && x < 6 && (arr[y, x] & 2) > 0 && (arr[y, x + 1] & 8) > 0)
-        {
-            if (x + 1 == 6)
-            {
-                return len;
-            }
-            else
-            {
-                return dfs(y, x + 1, 2, len + 1);
-            }
-        }
-
-        return 0;
-    }
-    /*
     private int dfs(int y, int x, int prev)
     {
-        // 이미 방문한 경우 탐색 종료
-        if (visited[y, x])
-        {
-            visited[y, x] = false;
-            return 0;
-        }
-
         // 현재 위치 방문 표시 및 경로 저장
         if (y != 0 && y != 6 && x != 0 && x != 6)
         {
-            visited[y, x] = true;
+            path.Add((y, x));
         }
-        path.Add((y, x));
-            
 
         if (prev != 1 && y < 6 && (arr[y, x] & 4) > 0 && (arr[y + 1, x] & 1) > 0)
         {
             if (y + 1 == 6)
             {
-                return path.Count - 1;
+                return path.Count;
 
             }
             else
@@ -165,7 +89,7 @@ public class pdefd77_BoardCheck : MonoBehaviour
         {
             if (x - 1 == 0)
             {
-                return path.Count - 1;
+                return path.Count;
             }
             else
             {
@@ -177,7 +101,7 @@ public class pdefd77_BoardCheck : MonoBehaviour
         {
             if (y - 1 == 0)
             {
-                return path.Count - 1;
+                return path.Count;
             }
             else
             {
@@ -189,30 +113,35 @@ public class pdefd77_BoardCheck : MonoBehaviour
         {
             if (x + 1 == 6)
             {
-                return path.Count - 1;
+                return path.Count;
             }
             else
             {
                 return dfs(y, x + 1, 2);
             }
         }
-
-        // 탐색 종료 후 방문한 경로 초기화
-        visited[y, x] = false;
-        path.RemoveAt(path.Count - 1);
-
-        path.Clear();
         return 0;
     }
-    */
 
     private void getScore(int len)
     {
         // 점수 계산 : 배율 정해서. 이부분은 쉽게 수정되게. 배율변수 빼기.
         displayedTileCount -= len;
         score += len * scoreMultiplier;
-        // 타일 파괴. path에 들어있는 값들을 이용해서 파괴.
-        // { 추후 코드추가 }
+        // 타일 파괴. path에 들어있는 값들을 이용해서 파괴. 추가로 path에 들어있는 인덱스 값들 이용해서 item획득까지.
+        foreach (var (y, x) in path)
+        {
+            destroyTile(y, x);
+            if (item[y, x] == 1)
+            {
+                //리롤+1
+            }
+            else if (item[y, x] == 2)
+            {
+                //타일제거
+            }
+            item[y, x] = 0;
+        }
     }
 
     private void destroyTile(int y, int x)

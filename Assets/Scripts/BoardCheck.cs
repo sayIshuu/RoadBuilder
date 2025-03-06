9a8d7c860ee3623a076d62c125c992714247a252
pdefd77 <pdefd77@gmail.com>
2025-03-06 11:54:38 +0900

    사이클 확인 코드 전면 수정
    DFS에서 UF로 변경

--- Assets/Scripts/BoardCheck.cs
+++ Assets/Scripts/BoardCheck.cs
@@ -18,13 +18,9 @@ public class BoardCheck : MonoBehaviour
     public int displayedTileCount = 0;
     //dfs추적을 위한 list. 각 int값으로 adj의 인덱스값이 들어갑니다.
     private List<(int, int)> path = new List<(int, int)>();
-<<<<<<< Updated upstream
-    //어떤 아이템을 그 칸에 들어있는지 나타내는 배열. 0 : 빈칸, 1 : 리롤+1, 2 : 타일제거
-    private int[,] item = new int[7, 7];
-=======
+
     private int circuitcount = 0;
     private int[] uf = new int[49];
->>>>>>> Stashed changes
 
     public static int[,] adj = new int[7, 7];
 
@@ -71,7 +67,6 @@ public class BoardCheck : MonoBehaviour
                     if (UfFind(7 * i + j) == 0 && UfFind(7 * i + j - 1) == 0) isCycle = true;
                     else UfMerge(7 * i + j, 7 * i + j - 1);
                 }
-                Debug.Log(uf[7 * i + j]); Debug.Log(uf[7 * i + j-7]); Debug.Log(uf[7 * i + j+1]); Debug.Log(uf[7 * i + j+7]); Debug.Log(uf[7 * i + j-1]);
             }
         }
 
@@ -144,114 +139,6 @@ public class BoardCheck : MonoBehaviour
         displayedTileCount -= len;
         score += len * len * len;
     }
-    public void Check()
-    {
-        for (int i = 0; i < 7; i++)
-        {
-            for (int j = 0; j < 7; j++)
-            {
-                if (i != 0 && i != 6 && j != 0 && j != 6) continue;
-
-                int val = Dfs(i, j, 0);
-
-                if (val > 0)
-                {
-                    GetScore(val);
-                }
-                if (displayedTileCount >= 25)
-                {
-                    //gameOverTxt.text = "Your Score is " + score;
-                    gameover = true;
-                }
-                
-            }
-        }
-        //턴 증가
-        TurnCounting.Instance.turnCount++;
-        //턴에 해당하는 점수 충족 여부 확인 및 게임 종료 결정
-        TurnCounting.Instance.CheckTrunAndGoal();
-
-        if (gameover)
-        {
-            SoundManager.Instance.PlayGameOverSound();
-            gameOverTxt.gameObject.SetActive(true);
-            gameOverTxt.text = "Your Score is " + score;
-        }
-
-        scoreTxt.text = "Score : " + score;
-    }
-
-
-    private int Dfs(int y, int x, int prev)
-    {
-        // 현재 위치 방문 표시 및 경로 저장
-        if (y != 0 && y != 6 && x != 0 && x != 6)
-        {
-            path.Add((y, x));
-        }
-
-<<<<<<< Updated upstream
-        if (prev != 1 && y < 6 && (arr[y, x] & 4) > 0 && (arr[y + 1, x] & 1) > 0)
-=======
-        circuitcount++;
-        if(circuitcount > 1000)
-        {
-            path.Clear();
-            return -1;
-        }
-
-        if (prev != 1 && y < 6 && (adj[y, x] & 4) > 0 && (adj[y + 1, x] & 1) > 0)
->>>>>>> Stashed changes
-        {
-            if (y + 1 == 6)
-            {
-                return path.Count;
-
-            }
-            else
-            {
-                return Dfs(y + 1, x, 4);
-            }
-        }
-
-        if (prev != 2 && x > 0 && (adj[y, x] & 8) > 0 && (adj[y, x - 1] & 2) > 0)
-        {
-            if (x - 1 == 0)
-            {
-                return path.Count;
-            }
-            else
-            {
-                return Dfs(y, x - 1, 8);
-            }
-        }
-
-        if (prev != 4 && y > 0 && (adj[y, x] & 1) > 0 && (adj[y - 1, x] & 4) > 0)
-        {
-            if (y - 1 == 0)
-            {
-                return path.Count;
-            }
-            else
-            {
-                return Dfs(y - 1, x, 1);
-            }
-        }
-
-        if (prev != 8 && x < 6 && (adj[y, x] & 2) > 0 && (adj[y, x + 1] & 8) > 0)
-        {
-            if (x + 1 == 6)
-            {
-                return path.Count;
-            }
-            else
-            {
-                return Dfs(y, x + 1, 2);
-            }
-        }
-        path.Clear();
-        return 0;
-    }
 
     private void GetScore(int len)
     {
@@ -267,15 +154,11 @@ public class BoardCheck : MonoBehaviour
 
     private void DestroyTile(int y, int x)
     {
-<<<<<<< Updated upstream
-        arr[y, x] = 0;
-        boardSlot[5 * y + x - 6].transform.GetChild(0).GetComponent<TileDestroy>().StartBreak();
-=======
+
         adj[y, x] = 0;
         if(boardSlot[5 * y + x - 6].transform.childCount > 0)
         {
             boardSlot[5 * y + x - 6].transform.GetChild(0).GetComponent<TileDestroy>().StartBreak();
         }
->>>>>>> Stashed changes
     }
 }

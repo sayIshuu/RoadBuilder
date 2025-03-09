93e60341c93df496ef10eecfab8457e833e7688f
sayIshuu <smile52673@gmail.com>
2025-03-09 17:32:58 +0900

    최고기록 BestScore 저장되게끔 구현

--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -4,26 +4,33 @@ using UnityEngine;
 public class ScoreManager : MonoBehaviour
 {
     public static int score = 0;
-    public int BestScore = 0;
     [SerializeField] private TextMeshProUGUI scoreTxt;
+
+    private int bestScore = 0;
+    [SerializeField] private TextMeshProUGUI bestScoreTxt;
 
     private void Awake()
     {
         score = 0;
         scoreTxt.text = score.ToString();
+        bestScore = PlayerPrefs.GetInt("BestScore", 0);
+        bestScoreTxt.text = bestScore.ToString();
     }
 
     public void AddScore(int value)
     {
         score += value;
         scoreTxt.text = score.ToString();
+        saveBestScore();
     }
 
-    public void checkBestScore(int value)
+    public void saveBestScore()
     {
-        if (BestScore < value)
+        if(score > bestScore)
         {
-            BestScore = value;
+            bestScore = score;
+            PlayerPrefs.SetInt("BestScore", bestScore);
         }
+        bestScoreTxt.text = bestScore.ToString();
     }
 }

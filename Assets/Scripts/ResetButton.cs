514168ce1299b495d1cab148351554cfca94892b
sayIshuu <smile52673@gmail.com>
2025-10-31 12:35:02 +0900

    [MOD] 목숨기능제거
    [MOD] 목숨기능제거

--- Assets/Scripts/ResetButton.cs
+++ Assets/Scripts/ResetButton.cs
@@ -6,13 +6,13 @@ public class ResetButton : MonoBehaviour
 {
     public void Reset()
     {
-        // 목숨부족시 재시작 불가.
-        if (LifeManager.Instance.CurrentLives <= 0)
-        {
-            // todo : 광고유도 - 동영님
-            // 광고보면 LifeManager.Instance.RecoverAllLife(); 호출
-            return;
-        }
+        //// 목숨부족시 재시작 불가.
+        //if (LifeManager.Instance.CurrentLives <= 0)
+        //{
+        //    // todo : 광고유도 - 동영님
+        //    // 광고보면 LifeManager.Instance.RecoverAllLife(); 호출
+        //    return;
+        //}
 
         SoundManager.Instance.PlaySelectSound();
         SceneManager.LoadScene("MainScene");

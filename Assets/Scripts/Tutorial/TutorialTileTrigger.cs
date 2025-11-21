d7c2562798b8c7b5f402b0d6ff80851c5c260c2b
zodang <78644385+zodang@users.noreply.github.com>
2025-11-22 01:13:19 +0900

    [FIX] 튜토리얼 중 리롤 시 발생하는 오류 수정
    - 리롤 시 타일 배치 감지 이벤트 누락 수정

--- Assets/Scripts/Tutorial/TutorialTileTrigger.cs
+++ Assets/Scripts/Tutorial/TutorialTileTrigger.cs
@@ -5,11 +5,31 @@ public class TutorialTileTrigger : TutorialBase
 {
     private TileDraggable[] tileDraggables;
     private TutorialController _controller;
-    private bool _isSet;
+    private TutorialRerollButton _rerollBtn;
+    private bool _completed = false;
 
     public override void Enter(TutorialController controller)
     {
         _controller = controller;
+        _rerollBtn = FindAnyObjectByType<TutorialRerollButton>();
+        _rerollBtn.OnClicked += AddDetectPlacementEvent;
+
+        AddDetectPlacementEvent();
+    }
+
+    public override void Execute(TutorialController controller)
+    {
+    }
+
+    public override void Exit()
+    {
+        UnsubscribeTileEvents();
+        _rerollBtn.OnClicked -= AddDetectPlacementEvent;
+    }
+
+    private void AddDetectPlacementEvent()
+    {
+        UnsubscribeTileEvents();
 
         tileDraggables = FindObjectsByType<TileDraggable>(FindObjectsSortMode.None);
 
@@ -19,20 +39,26 @@ public class TutorialTileTrigger : TutorialBase
         }
     }
 
-    public override void Execute(TutorialController controller)
+    private void UnsubscribeTileEvents()
     {
-    }
+        if (tileDraggables == null) return;
 
-    public override void Exit()
-    {
         for (int i = 0; i < tileDraggables.Length; i++)
         {
-            tileDraggables[i].OnPlaced -= DetectPlacement;
+            if (tileDraggables[i] != null)
+            {
+                tileDraggables[i].OnPlaced -= DetectPlacement;
+            }
         }
+
+        tileDraggables = null;
     }
 
     private void DetectPlacement()
     {
+        if (_completed) return;
+        _completed = true;
+
         _controller.SetNextTutorial();
     }
 }
